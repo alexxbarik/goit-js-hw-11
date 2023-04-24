08114c0ca932093f2b7ea2b905638bc67bc652e7
Oleksandr Barabash <alexxbarik@gmail.com>
2023-04-24 22:55:02 +0300

    зміни 24.04.23 22.54

--- src/index.js
+++ src/index.js
@@ -43,19 +43,19 @@ if(res.hits.length === 0){
 function makeCard(res){
     const markup =res.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
     `<div class="photo-card">
-    <a href=${largeImageURL}><img src=${webformatURL} alt="${tags}" loading="lazy" /></a>
+    <a class="photo-card-link"href=${largeImageURL}><img src=${webformatURL} alt="${tags}" loading="lazy" /></a>
     <div class="info">
       <p class="info-item">
-        <b>likes:${likes}</b>
+        <b>likes${likes}</b>
       </p>
       <p class="info-item">
-        <b>views:${views}</b>
+        <b>views${views}</b>
       </p>
       <p class="info-item">
-        <b>comments:${comments}</b>
+        <b>comments${comments}</b>
       </p>
       <p class="info-item">
-        <b>downloads:${downloads}</b>
+        <b>downloads${downloads}</b>
       </p>
     </div>
   </div>`).join("");
